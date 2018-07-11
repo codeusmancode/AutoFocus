020685848a910e6c7ecbe6799a8ac276998acccf
usmanriaz <usmanriaz@UsmanRiaz.usaparel.com>
2018-07-11 13:03:51 +0500

    added bean

---
+++ ViewController/src/view/gg.java
@@ -0,0 +1,12 @@
+package view;
+
+public class gg {
+    public gg() {
+    }
+
+    public String b1_action() {
+        // Add event code here...
+        //usman riaz working
+        return null;
+    }
+}
